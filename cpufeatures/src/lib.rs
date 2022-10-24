0d26dfdcf1d84d64a0cada8031fa755cf0d18d87
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-10-24 13:27:29 +0300

    tmp

--- cpufeatures/src/lib.rs
+++ cpufeatures/src/lib.rs
@@ -71,8 +71,8 @@ mod x86;
 #[cfg(miri)]
 mod miri;
 
-#[cfg(not(any(target_arch = "aarch64", target_arch = "x86", target_arch = "x86_64")))]
-compile_error!("This crate works only on `aarch64`, `x86`, and `x86-64` targets.");
+//#[cfg(not(any(target_arch = "aarch64", target_arch = "x86", target_arch = "x86_64")))]
+//compile_error!("This crate works only on `aarch64`, `x86`, and `x86-64` targets.");
 
 /// Create module with CPU feature detection code.
 #[macro_export]
